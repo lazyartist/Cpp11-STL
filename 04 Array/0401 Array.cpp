ff71c9cdfe9201969b9c01a1a97a239418a71262
lazyartist <uuux3@yahoo.co.kr>
2019-02-07 22:33:35 +0900

    Array

--- 04 Array/0401 Array.cpp
+++ 04 Array/0401 Array.cpp
@@ -17,15 +17,41 @@ int main() {
 		cout << arr[1] << endl; // 0
 	}
 
-	cout << "반복자 사용" << endl;
+	cout << "반복자 사용 : std::array<...>::iterator" << endl;
 	{
 		array<int, 3> arr = { 9, 1 };
 		for (array<int, 3U>::iterator iter = arr.begin(); iter < arr.end(); iter++)
 		{
 			cout << *iter << endl; // 9, 1, 0
 		}
+	}
 
-		int a = 3u;
+	cout << "첫 번째 요소 : front()" << endl;
+	cout << "지정한 인덱스에 접근 : at()" << endl;
+	cout << "마지막 요소 : back()" << endl;
+	cout << "배열 크기 : size()" << endl;
+	cout << "배열 데이터에 직접 접근 : data()" << endl;
+	{
+		array<int, 3> arr = { 1, 2, 3 };
+		cout << arr.front() << endl; // 1
+		cout << arr.at(1) << endl; // 2
+		cout << arr.back() << endl; // 3
+
+		int* pArr = arr.data();
+		for (size_t i = 0; i < arr.size(); i++)
+		{
+			cout << pArr[i] << endl; // 1, 2, 3
+		}
+	}
+
+	cout << "특정 값으로 채우기 : fill()" << endl;
+	{
+		array<int, 3> arr = { 1, 2, 3 };
+		arr.fill(9);
+		for (array<int, 3U>::iterator iter = arr.begin(); iter < arr.end(); iter++)
+		{
+			cout << *iter << endl; // 9, 9, 9
+		}
 	}
 
 	return 0;
